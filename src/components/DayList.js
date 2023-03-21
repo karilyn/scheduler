abf197ef4b48481469839787063cf073a99d561b
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-21 14:43:01 -0700

    Modify DayListItem return to accept props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,22 +1,27 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
+// DayList accepts 3 props:
+// 1. [days]: an array of day objects, each containing an id, name and spots
+// 2. day (a string, the currently selected day)
+// 3. setDay function which accepts the day
 export default function DayList(props) {
 
+  // The <DayList> renders and passes props to the <DayListItem> children causing the updates to the selected visual state.
   const days = props.days.map((day) => {
     return (
       <DayListItem
         key={day.id}
         name={day.name}
         spots={day.spots}
-        selected={day.name === day}
-        setDay={day.setDay}
+        selected={day.name === props.day}
+        setDay={props.setDay}
       />
     )
   });
 
   return (
-    <ul className="days">
+    <ul>
       {days}
     </ul>
   )
