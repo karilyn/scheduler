9a2aee0a90e3b3c5c33a92654853729f1c5f950a
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-25 15:00:28 -0700

    Add <Confirm> component and its story

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -150,10 +153,17 @@ storiesOf("Appointment", module)
       onAdd={action("onAdd")}
     />
   ))
-  add("Show", () => (
+  .add("Show", () => (
     <Show
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message=""
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
     />
   ))
 
