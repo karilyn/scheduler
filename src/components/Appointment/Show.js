76265d7c91b96a40b50e978e1615cd0a1e3b7b95
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-25 14:39:31 -0700

    Add <Show> component

---
+++ src/components/Appointment/Show.js
@@ -0,0 +1,38 @@
+import React, { className } from "react";
+
+// accepts four props:
+// 1) student:String
+// 2) interviewer:Object
+// 3) onEdit
+// 4) onDelete
+
+export default function Show(props) {
+  return (
+    <main className="appointment__card appointment__card--show">
+      <section className="appointment__card-left">
+        <h2 className="text--regular">Lydia Miller-Jones</h2>
+        <section className="interviewer">
+          <h4 className="text--light">Interviewer</h4>
+          <h3 className="text--regular">Sylvia Palmer</h3>
+        </section>
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <img
+            className="appointment__actions-button"
+            src="images/edit.png"
+            alt="Edit"
+            onClick={props.onEdit}
+          />
+          <img
+            className="appointment__actions-button"
+            src="images/trash.png"
+            alt="Delete"
+            onClick={props.onDelete}
+          />
+        </section>
+      </section>
+    </main>
+
+  )
+}
