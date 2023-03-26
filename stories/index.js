7de99a707e43c8ce036b33c43c14a3b31cb15d7d
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-25 22:39:21 -0700

    Add 'Appointment Booked' story

--- stories/index.js
+++ stories/index.js
@@ -192,4 +192,15 @@ storiesOf("Appointment", module)
       <Appointment time="5pm" />
     </Fragment>
   ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+
 
