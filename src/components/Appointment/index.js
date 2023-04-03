7f20ea91f480171e1b1eb187232a34b38225afaa
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-04-03 14:27:48 -0700

    Add save function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,15 +5,34 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
+import Status from "./Status";
 
 
 
 
 export default function Appointment(props) {
+  // visual mode constants used to setMode
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    transition(SAVING);
+    // new interview object gets passed to bookInterview function
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+    console.log("bookInterview:", props.id, interview)
+
+  }
+
+  // import functions used to transition to different visual modes and set mode to SHOW or EMPTY depending on if interview is booked
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   )
@@ -30,8 +49,14 @@ export default function Appointment(props) {
       )}
       {mode === CREATE && (
         <Form
-          interviewers={[]}
+          interviewers={props.interviewers}
           onCancel={() => back()}
+          onSave={save}
+        />
+      )}
+      {mode === SAVING && (
+        <Status
+          message="Saving appointment"
         />
       )}
 
