6ac9cf91d10054dd43bcb3fefd10d0d2221cd894
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-04-06 13:36:07 -0700

    Bug fix in SET_INTERVIEW Added conditional into SET_INTERVIEW to only spread the object if it's not null

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,4 +1,4 @@
-import { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from "./actions";
+import { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from './actions';
 
 /* Function to increment or decrement spots remaining
 We will return the result of mapping over each dayObj in state.days
@@ -6,54 +6,61 @@ start by finding the specific day when the names match, then
 loop over the appointmentKeys in appointments
 */
 export function updateSpots(state, appointments) {
-
+  // console.log("appointments: ", appointments)
   return state.days.map((dayObj) => {
     // console.log("state.days", state.days)
+    if (state.day === dayObj.name) {
+      let spotsRemaining = 0;
+      for (const appointmentKey in appointments) {
+        let appointment = appointments[appointmentKey];
+        // add to spotsRemaining if the appointment.id is in dayObj's appointments and it's null
+        console.log("appointment ", appointment)
 
-  if (state.day === dayObj.name) {
-    let spotsRemaining = 0;
-    for (const appointmentKey in appointments) {
-      let appointment = appointments[appointmentKey];
-      // add to spotsRemaining if the appointment.id is in dayObj's appointments and it's null
-      if (dayObj.appointments.includes(appointment.id) && !appointment.interview) {
-        // console.log("appointment found:", appointment)
-        spotsRemaining++;
+        if (
+          dayObj.appointments.includes(appointment.id) &&
+          !appointment.interview
+        ) {
+          // console.log("appointment  found:", appointment)
+          spotsRemaining++;
+        }
       }
+      // return a new dayObject copy with an updated spots value
+      // console.log("spots remaining: ", spotsRemaining)
+      return { ...dayObj, spots: spotsRemaining };
     }
-    // return a new dayObject copy with an updated spots value
-    return {...dayObj, spots: spotsRemaining};
-  }
-  // console.log("dayObj: ", dayObj)
-  return dayObj;
-  })
+    // console.log("dayObj: ", dayObj)
+    return dayObj;
+  });
 }
 
-export function reducer (state, action) {
+export function reducer(state, action) {
   switch (action.type) {
     case SET_DAY:
       return {
         ...state,
-        day: action.value
+        day: action.value,
       };
     case SET_APPLICATION_DATA:
       return {
         ...state,
-        ...action.value
+        ...action.value,
       };
     case SET_INTERVIEW:
+      console.log(action);
       const appointment = {
-        ...state.appointments[action.id],
-        interview: { ...action.interview }
+        ...state.appointments[action.value.id],
+        interview: action.value.interview && { ...action.value.interview },
       };
       const appointments = {
         ...state.appointments,
-        [action.id]: appointment
+        [action.value.id]: appointment,
       };
-      const days = updateSpots(state, appointments)
+      const days = updateSpots(state, appointments);
+      console.log("days: ", days)
       return {
         ...state,
         days,
-        appointments
+        appointments,
       };
     default:
       throw new Error(
@@ -61,4 +68,3 @@ export function reducer (state, action) {
       );
   }
 }
-
