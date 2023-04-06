a34ffdbee4e8e79e597cd508677075cc14a70c02
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-04-05 22:39:01 -0700

    Import useReducer functionality, working partially

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,57 +1,39 @@
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import axios from "axios";
+import reducer, { SET_APPLICATION_DATA, SET_DAY, SET_INTERVIEW, updateSpots } from "reducers/reducers";
+
 
 // this hook controls the data management
 export default function useApplicationData() {
-  const [state, setState] = useState({
-    day: "Monday",
+
+  const [state, dispatch] = useReducer(reducer, {
+    day: 'Monday',
     days: [],
     appointments: {},
     interviewers: {}
-  })
+  });
+
+  // setDay updates the state for an individual day in our object of states
+  const setDay = (day) => dispatch({ type: SET_DAY, value: day });
 
   useEffect(() => {
-    Promise.all([
+    Promise
+    .all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
       axios.get('/api/interviewers'),
     ])
     .then(all => {
-      setState(prev => ({
-        ...prev,
-        days:all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      }))
-    })
-  }, []);
-
-  // setDay updates the state for an individual day in our object of states
-  const setDay = (day) => setState({ ...state, day });
-
-
-  function updateSpots(appointments) {
-    // we will return the result of mapping over each dayObj in state.days
-    return state.days.map((dayObj) => {
-      // start by finding the specific day when the names match
-      if (state.day === dayObj.name) {
-        let spotsRemaining = 0;
-        // loop over the appointmentKeys in appointments
-        for (const appointmentKey in appointments) {
-          let appointment = appointments[appointmentKey];
-          // add to spotsRemaining if the appointment.id is in dayObj's appointments and it's null
-          if (dayObj.appointments.includes(appointment.id) && !appointment.interview) {
-            console.log("appointment found:", appointment)
-            spotsRemaining++;
-          }
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        value: {
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
         }
-        // return a new dayObject copy with an updated spots value
-        return {...dayObj, spots: spotsRemaining};
-      }
-      return dayObj;
-    })
-  }
-
+      });
+    });
+  }, []);
 
 
   // call bookInterview() when a user creates an interview
@@ -65,21 +47,24 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-    // call the updateSpots function to get a new dayObj copy and then update the state with it
-    const days = updateSpots(appointments)
+    // call the updateSpots function to get a new dayObj copy and then update the st
+    const days = updateSpots(state, appointments)
 
-    console.log("days:", days)
+
     // make the put request and then update the state
     return axios
       .put(`/api/appointments/${id}`, {
-      interview
-    })
-      .then((res) => setState({
-      ...state,
-      appointments,
-      days
-
-    }));
+        interview,
+      })
+      .then((res) => {
+        dispatch({
+          type: SET_INTERVIEW,
+          value: {
+            appointments,
+            days
+          }
+        });
+      });
   }
 
   function deleteInterview(id) {
@@ -91,14 +76,18 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-    const days = updateSpots(appointments)
+    const days = updateSpots(state, appointments)
     return axios
       .delete(`/api/appointments/${id}`)
-      .then((res) => setState({
-      ...state,
-      appointments,
-      days
-    }));
+      .then((res) => {
+        dispatch({
+          type: SET_INTERVIEW,
+          value: {
+            appointments,
+            days
+          }
+        })
+    });
   }
 
   return { state, setDay, bookInterview, deleteInterview }
