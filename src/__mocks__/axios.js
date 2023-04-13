23496e17215df36f9d2cd9ecc50046a957e68cdf
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-04-13 14:16:41 -0700

    Remove a console log

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -58,7 +58,6 @@ const fixtures = {
 export default {
   defaults: { baseURL: '' },
   get: jest.fn((url) => {
-    console.log('url: ', url);
     if (url === '/api/days') {
       return Promise.resolve({
         status: 200,
